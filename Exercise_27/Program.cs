8ef509c20e3bb392d791e804f1f12b35e0a481b3
David Gugea <davidgugea@yahoo.com>
2020-10-10 22:29:41 +0200

    Exercise 27 done.

---
+++ Exercise_27/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Exercise_27
+{
+    class Program
+    {
+        static int return_digits_sum(int number)
+        {
+            char[] digits = number.ToString().ToCharArray();
+            int sum = 0;
+            foreach(char digit in digits)
+            {
+                sum += Int32.Parse(digit.ToString());
+            }
+
+            return sum;
+        }
+        static void Main(string[] args)
+        {
+            /*
+             * 27. Write a C# program and compute the sum of the digits of an integer. Go to the editor
+            Sample Output:
+            Input a number(integer): 12
+            Sum of the digits of the said integer: 3
+            */
+
+            Console.WriteLine(return_digits_sum(12));
+        }
+    }
+}
