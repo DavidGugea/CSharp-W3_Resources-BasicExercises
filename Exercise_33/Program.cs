10a3e92d499ac4bbf7064e9a230703b20065794b
David Gugea <davidgugea@yahoo.com>
2020-10-11 22:57:50 +0200

    Exercise 33 done.

---
+++ Exercise_33/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Exercise_33
+{
+    class Program
+    {
+        static bool m_3_or_7(int number)
+        {
+            return number % 3 == 0 || number % 7 == 0 ? true : false;
+        }
+        static void Main(string[] args)
+        {
+            /*
+             * 33. Write a C# program to check if a given positive number is a multiple of 3 or a multiple of 7. Go to the editor
+            Sample Output:
+            Input first integer:
+            15
+            True
+            */
+
+            Console.WriteLine(m_3_or_7(15));
+        }
+    }
+}
