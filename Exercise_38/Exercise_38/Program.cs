a8834e7df2febfa439d9eb124c7fa884aa771837
= <=>
2020-10-15 12:24:30 +0200

    Exercise 38 done.

---
+++ Exercise_38/Exercise_38/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_38
+{
+    class Program
+    {
+        static string solution(string str)
+        {
+            return str.Substring(0, 2);
+        }
+        static void Main(string[] args)
+        {
+            /*
+             * 38. Write a C# program to get a new string of two characters from a given string. The first and second character of the given string must be "P" and "H", so PHP will be "PH". Go to the editor
+            Test Data: PHP
+            Sample Output:
+            PH
+            */
+
+            Console.WriteLine(solution("PHP Tutorial"));
+            Console.ReadKey();
+        }
+    }
+}
