91d386f9f9e7859cad70ee0585919ee716503908
= <=>
2020-10-15 12:58:08 +0200

    Exercise 43 done.

---
+++ Exercise_43/Exercise_43/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_43
+{
+    class Program
+    {
+        static string askForString(out string str)
+        {
+            Console.WriteLine("Input a string : ");
+            str = Console.ReadLine();
+
+            return str;
+        }
+        static bool solution()
+        {
+            string str;
+            str = askForString(out str);
+            return str.StartsWith("wwwww");
+        }
+        static void Main(string[] args)
+        {
+            /*
+             * 43. Write a C# program to check if a given string starts with "w" and immediately followed by two "ww". Go to the editor
+            Test Data:
+            Input a string : www
+            Sample Output
+            False
+            */
+
+            Console.WriteLine(solution());
+            Console.ReadKey();
+        }
+    }
+}
