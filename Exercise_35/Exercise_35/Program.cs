c9180f5a0c97eb7fd0268ea883ba4bb5ebadb54f
= <=>
2020-10-15 12:08:57 +0200

    Exercise 35 done.

---
+++ Exercise_35/Exercise_35/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_35
+{
+    class Program
+    {
+        static void errorMessage(string msg)
+        {
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(" ");
+            }
+
+            Console.WriteLine(msg);
+            Console.WriteLine("Try again !");
+
+            for(int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(" ");
+            }
+        }
+        static void askForNumber(out int number, string user_request)
+        {
+            while (true)
+            {
+                Console.WriteLine(user_request);
+                try
+                {
+                    number = Int32.Parse(Console.ReadLine());
+                    break;
+                }
+                catch
+                {
+                    errorMessage("You have to input a number.");
+                    continue;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            /*
+             * 35. Write a C# program to check two given numbers where one is less than 100 and other is greater than 200. Go to the editor
+            Sample Output:
+            Input a first number(<100): 75
+            Input a second number(>100): 250
+            True
+            */
+
+            int first_number, second_number;
+            askForNumber(out first_number, "Input a first number(<100): ");
+            askForNumber(out second_number, "Input a second number(>100): ");
+
+            if(first_number < 100 && second_number > 100)
+            {
+                Console.WriteLine("True");
+            }
+            else
+            {
+                Console.WriteLine("False");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise_35/Exercise_35/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Exercise_35")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exercise_35")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("79f0f158-e981-4ce2-a22d-15001e4d33e4")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
