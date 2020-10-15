cde4540aadc66b4497a0f930fc8bb46b9d27b9cc
= <=>
2020-10-15 12:52:22 +0200

    Exercise 42 done.

---
+++ Exercise_42/Exercise_42/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exercise_42
+{
+    class Program
+    {
+        static string solution(string str)
+        {
+            if(str.Length <= 3)
+            {
+                return str.ToUpper();
+            }
+            else
+            {
+                return str.Substring(0, 4).ToLower() + str.Substring(4);
+            }
+        }
+        static void Main(string[] args)
+        {
+            /*
+             * 42. Write a C# program to create a new string where the first 4 characters will be in lower case. If the string is less than 4 characters then make the whole string in upper case. Go to the editor
+            Test Data:
+            Input a string: w3r
+            Sample Output
+            W3R
+            */
+
+            Console.WriteLine(solution("w3r"));
+            Console.WriteLine(solution("ASDF"));
+            Console.WriteLine(solution("ABCDefgh"));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Exercise_42/Exercise_42/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Allgemeine Informationen über eine Assembly werden über die folgenden
+// Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
+// die einer Assembly zugeordnet sind.
+[assembly: AssemblyTitle("Exercise_42")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Exercise_42")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Durch Festlegen von ComVisible auf FALSE werden die Typen in dieser Assembly
+// für COM-Komponenten unsichtbar.  Wenn Sie auf einen Typ in dieser Assembly von
+// COM aus zugreifen müssen, sollten Sie das ComVisible-Attribut für diesen Typ auf "True" festlegen.
+[assembly: ComVisible(false)]
+
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("514721d9-13f2-445d-9c36-a2833f0c23b0")]
+
+// Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
+//
+//      Hauptversion
+//      Nebenversion
+//      Buildnummer
+//      Revision
+//
+// Sie können alle Werte angeben oder Standardwerte für die Build- und Revisionsnummern verwenden,
+// übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
