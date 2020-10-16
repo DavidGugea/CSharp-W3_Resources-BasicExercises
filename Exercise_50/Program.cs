bc0c98f4f4ac7ae8ac0cbb0d9adf246af8dc138c
David Gugea <davidgugea@yahoo.com>
2020-10-16 22:51:53 +0200

    Exercise 50 done.

---
+++ Exercise_50/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections;
+
+namespace Exercise_50
+{
+    class Program
+    {
+        static int[] rotate(int[] array)
+        {
+            return new int[3]
+            {
+                array[1],
+                array[2],
+                array[0]
+            };
+        }
+        static void Main(string[] args)
+        {
+            /*
+             * 50. Write a C# program to rotate an array (length 3) of integers in left direction. Go to the editor
+            Test Data:
+            Array1: [1, 2, 8]
+            After rotating array becomes: [2, 8, 1]
+            */
+
+            int[] array = new int[] { 1, 2, 8 };
+
+            int[] solution = rotate(array);
+            IEnumerator enumerator = solution.GetEnumerator();
+            while (enumerator.MoveNext())
+            {
+                Console.WriteLine(enumerator.Current);
+            }
+        }
+    }
+}
