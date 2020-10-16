3f8108ee5c9ba048fc9cb9f6c56a27a24c75b87b
David Gugea <davidgugea@yahoo.com>
2020-10-16 23:09:00 +0200

    Exercise 54 done.

---
+++ Exercise_54/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise_54
+{
+    class Program
+    {
+        static int get_century(double year)
+        {
+            return Convert.ToInt32(Math.Ceiling(year / 100));
+        }
+        static void Main(string[] args)
+        {
+            /*
+             * 54. Write a C# program to get the century from a year. 
+            */
+
+            Console.WriteLine(get_century(2020));
+        }
+    }
+}
