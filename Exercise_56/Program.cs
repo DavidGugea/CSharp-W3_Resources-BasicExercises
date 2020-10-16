1e8b39575fdad4e69335b17d7efcff40d51a90ce
David Gugea <davidgugea@yahoo.com>
2020-10-16 23:32:33 +0200

    Exercise 56 done.

---
+++ Exercise_56/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace Exercise_56
+{
+    class Program
+    {
+        static bool is_palindrome(string str)
+        {
+            return str== String.Concat(str.Reverse()) ? true : false;
+        }
+        static void Main(string[] args)
+        {
+            /*
+             * 56. Write a C# program to check if a given string is a palindrome or not. Go to the editor
+            Sample Example:
+            For 'aaa' the output should be true
+            For 'abcd' the output should be false
+            */
+
+            Console.WriteLine(is_palindrome("aaa"));
+            Console.WriteLine(is_palindrome("abcd"));
+        }
+    }
+}
